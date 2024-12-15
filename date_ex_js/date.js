d28bcc4f89b23b6cc966c7fce44576cf1a79e1ee
khadkaxashish <khadkaxashish@gmail.com>
2024-12-15 01:53:31 -0700

    clean up, delete extra files

--- date_ex_js/date.js
+++
@@ -1,5 +0,0 @@
-console.log(new Date());
-console.log(new Date().toLocaleTimeString());
-const localDateTime = new Date().toLocaleString();
-console.log(localDateTime);
-
--- generate_data_set/read_all.sh
+++
@@ -1,2 +0,0 @@
- curl -X GET http://localhost:3000/sensor/readAll
-
